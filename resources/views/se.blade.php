5b287103846aa8128a5157c6294e5bde849fd83c
Raj Krishnan R <rkrishnan1993@live.com>
2021-02-12 10:02:05 +0400

    rapid fix

--- resources/views/se.blade.php
+++ resources/views/se.blade.php
@@ -32,7 +32,7 @@
         <video class="videoSlot" autoplay id="tv"></video>
 
     </div>
-        <video id="viewfinder"></video>
+        <video id="viewfinder" autoplay muted></video>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
